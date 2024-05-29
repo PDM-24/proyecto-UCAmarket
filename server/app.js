ceac1cfddfc7275e476933e67b228839fcb1011f
Daniel Juarez <00012922@uca.edu.sv>
2024-05-28 20:05:04 -0600

    Update: backend UNISHOP

--- server/app.js
+++ server/app.js
@@ -2,16 +2,24 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const db = require("./config/database.config")
+const database = require("./config/database.config");
+const apiRouter = require("./routes/index.router")
 
 const app = express();
-db.connect();
+database.connect();
 
+//logger for request
 app.use(logger('dev'));
+
+//body parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//static router
 app.use(express.static(path.join(__dirname, 'public')));
 
+// API Router
+app.use("/api/v1", apiRouter);
 
 module.exports = app;

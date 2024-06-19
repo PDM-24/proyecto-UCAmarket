1c8756b7508a04a36280046a23de7045ca13f43a
KarlaD2120 <00061922@uca.edu.sv>
2024-06-18 20:17:22 -0600

    Agregar interfaz de pantalla de editar producto

--- app/src/main/java/com/delgadojuarez/ucamarket/ui/screens/EditProduct.kt
+++ app/src/main/java/com/delgadojuarez/ucamarket/ui/screens/EditProduct.kt
@@ -1,11 +1,8 @@
 package com.delgadojuarez.ucamarket.ui.screens
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,12 +10,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -29,7 +23,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -37,54 +30,49 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.delgadojuarez.ucamarket.MainViewModel
+import com.delgadojuarez.ucamarket.R
+import com.delgadojuarez.ucamarket.ui.component.AppButton
 import com.delgadojuarez.ucamarket.ui.component.CustomOutlinedTextField
 import com.delgadojuarez.ucamarket.ui.component.TopBar
-import com.delgadojuarez.ucamarket.ui.theme.InriaSans
-import com.delgadojuarez.ucamarket.ui.theme.azul
 import com.delgadojuarez.ucamarket.ui.theme.grisTextFields
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SignupScreen(
+fun EditProduct(
     viewModel: MainViewModel,
     navController: NavController
 ){
-    var nombre by remember {
-        mutableStateOf("")
-    }
-    var email by remember {
-        mutableStateOf("")
-    }
-    var telefono by remember {
-        mutableStateOf("")
-    }
-    var tipoUsuario by remember {
-        mutableStateOf("")
-    }
-    var nombreEmprendimiento by remember {
+    var nombreProducto by remember {
         mutableStateOf("")
     }
     var foto by remember {
         mutableStateOf("")
     }
+    var precio by remember {
+        mutableStateOf("")
+    }
+    var descripcion by remember {
+        mutableStateOf("")
+    }
 
     // Variables para Dropdown Menu
     var expanded by remember { mutableStateOf(false) }
-    val usuarios = listOf(
-        "Cliente",
-        "Emprendedor"
+    val categoria = listOf(
+        "Ropa y accesorios",
+        "Tecnología",
+        "Comida",
+        "Materiales de estudio",
+        "Arte y artesanía",
+        "Servicios",
+        "Otro"
     )
-    var selectedIndex by remember { mutableStateOf(usuarios[0]) }
-
-    val isReadOnly by derivedStateOf { selectedIndex != "Emprendedor" }
+    var selectedIndex by remember { mutableStateOf(categoria[0]) }
 
     Column(
         modifier = Modifier
@@ -94,61 +82,21 @@ fun SignupScreen(
         Spacer(modifier = Modifier.height(40.dp))
 
         TopBar(
-            title = "Registro",
+            title = "Agregar producto",
             navController = navController
         )
-
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
             CustomOutlinedTextField(
-                value = nombre,
-                onValueChange = { nombre = it },
-                label = "Nombre completo"
+                value = nombreProducto,
+                onValueChange = { nombreProducto = it },
+                label = "Nombre de producto"
             )
 
-            Spacer(modifier = Modifier.height(20.dp))
-
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text(text = "Correo electrónico") },
-                trailingIcon = { Icon(imageVector = Icons.Filled.MailOutline, contentDescription = "Email")},
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    containerColor = grisTextFields,
-                    cursorColor = Color.Black,
-                    focusedBorderColor = Color.Transparent,
-                    unfocusedBorderColor = Color.Transparent
-                ),
-                shape = RoundedCornerShape(12.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 30.dp)
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
-
-            OutlinedTextField(
-                value = telefono,
-                onValueChange = { telefono = it },
-                label = { Text(text = "Número de teléfono") },
-                trailingIcon = { Icon(imageVector = Icons.Filled.Phone, contentDescription = "Telefono")},
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    containerColor = grisTextFields,
-                    cursorColor = Color.Black,
-                    focusedBorderColor = Color.Transparent,
-                    unfocusedBorderColor = Color.Transparent
-                ),
-                shape = RoundedCornerShape(12.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 30.dp)
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
+            Spacer(modifier = Modifier.height(27.dp))
 
             ExposedDropdownMenuBox(
                 expanded = expanded,
@@ -163,7 +111,7 @@ fun SignupScreen(
                         .menuAnchor(),
                     value = selectedIndex,
                     onValueChange = {},
-                    label = { Text(text = "Tipo de usuario *") },
+                    label = { Text(text = "Categoría de producto") },
                     colors = TextFieldDefaults.outlinedTextFieldColors(
                         containerColor = grisTextFields,
                         cursorColor = Color.Black,
@@ -175,11 +123,11 @@ fun SignupScreen(
                     trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) }
                 )
                 ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-                    usuarios.forEachIndexed { index, usuario ->
+                    categoria.forEachIndexed { index, Categoria ->
                         DropdownMenuItem(
-                            text = { Text(text = usuario) },
+                            text = { Text(text = Categoria) },
                             onClick = {
-                                selectedIndex = usuarios[index]
+                                selectedIndex = categoria[index]
                                 expanded = false
                             },
                             contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
@@ -191,28 +139,10 @@ fun SignupScreen(
             Spacer(modifier = Modifier.height(20.dp))
 
             OutlinedTextField(
-                value = nombreEmprendimiento,
-                onValueChange = { if (!isReadOnly) nombreEmprendimiento = it },
-                label = { Text(text = "Nombre del emprendimiento") },
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    containerColor = if (isReadOnly) Color.LightGray else grisTextFields, // Cambia el color de fondo si está desactivado
-                    cursorColor = if (isReadOnly) Color.Transparent else Color.Black, // Oculta el cursor si está desactivado
-                    focusedBorderColor = if (isReadOnly) Color.Transparent else Color.Black, // Cambia el color del borde si está desactivado
-                    unfocusedBorderColor = if (isReadOnly) Color.Transparent else grisTextFields // Cambia el color del borde si está desactivado
-                ),
-                shape = RoundedCornerShape(12.dp),
-                readOnly = isReadOnly,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 30.dp)
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
-
-            OutlinedTextField(
-                value = foto,
-                onValueChange = { foto = it },
-                label = { Text(text = "Fotografía") },
+                value = precio,
+                onValueChange = { precio = it },
+                label = { Text(text = "Precio") },
+                trailingIcon = { Icon(painter = painterResource(id = R.drawable.baseline_attach_money_24), contentDescription = "Precio")},
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     containerColor = grisTextFields,
                     cursorColor = Color.Black,
@@ -225,34 +155,58 @@ fun SignupScreen(
                     .padding(horizontal = 30.dp)
             )
 
+            Spacer(modifier = Modifier.height(20.dp))
+
+            CustomOutlinedTextField(
+                value = foto,
+                onValueChange = { foto = it },
+                label = "Fotografía"
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            OutlinedTextField(
+                value = descripcion,
+                onValueChange = { descripcion = it },
+                label = { Text(text = "Descripcion") },
+                singleLine = false,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    containerColor = grisTextFields,
+                    cursorColor = Color.Black,
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 30.dp)
+                    .height(140.dp)
+            )
+
             Spacer(modifier = Modifier.height(128.dp))
             Spacer(modifier = Modifier.height(60.dp))
 
-            Button(onClick = {
-                Log.i("Credential","Email : $email Nombre : $nombre")
-            },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 50.dp)
-                    .height(50.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = azul),
-                elevation = ButtonDefaults.buttonElevation(5.dp)
-            ) {
-                Text(text = "Guardar")
-            }
+            AppButton(
+                text = "Actualizar",
+                onClick = {
+                    // Accion al dar click
+                }
+            )
+
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun SignupPreview() {
+fun EditProductPreview() {
+    // Aquí debes crear instancias ficticias de tus dependencias, como el viewModel y navController
+    // Para el preview, puedes pasar instancias vacías o mocks.
     val fakeViewModel = MainViewModel()
     val fakeNavController = rememberNavController()
 
-    SignupScreen(
+    EditProduct(
         viewModel = fakeViewModel,
         navController = fakeNavController
     )
 }
-

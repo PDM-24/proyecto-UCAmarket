c664a263548a8cbb00e2fc1cd31628e996367ae9
KarlaD2120 <00061922@uca.edu.sv>
2024-06-20 13:14:53 -0600

    Añadir pantalla de lista de deseos

---
+++ app/src/main/java/com/delgadojuarez/ucamarket/ui/screens/ListProducts.kt
@@ -0,0 +1,13 @@
+package com.delgadojuarez.ucamarket.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.delgadojuarez.ucamarket.MainViewModel
+
+@Composable
+fun ListProducts(
+    viewModel: MainViewModel,
+    navController: NavController
+){
+
+}

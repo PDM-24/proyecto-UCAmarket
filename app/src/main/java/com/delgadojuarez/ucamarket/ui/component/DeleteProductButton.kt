43344f02550e21284cd48014295bfdb321396069
KarlaD2120 <00061922@uca.edu.sv>
2024-06-20 14:07:56 -0600

    Añadir pantalla de lista de productos

---
+++ app/src/main/java/com/delgadojuarez/ucamarket/ui/component/DeleteProductButton.kt
@@ -0,0 +1,90 @@
+package com.delgadojuarez.ucamarket.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DeleteProductButton(onDeleteConfirmed: () -> Unit ={}) {
+    var showDialog by remember { mutableStateOf(false) }
+
+    Column {
+        Button(
+            onClick = { showDialog = true },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
+            elevation = ButtonDefaults.buttonElevation(5.dp),
+            modifier = Modifier.height(30.dp).padding(start = 10.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Delete,
+                contentDescription = "Eliminar",
+                tint = Color.White,
+                modifier = Modifier.size(35.dp)
+            )
+        }
+
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                title = { Text(
+                    text = "Confirmar eliminación",
+                    fontWeight = FontWeight.ExtraBold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(start = 15.dp, top = 5.dp)
+                ) },
+                text = {
+                    Column {
+                        Text(
+                            text = "El producto seleccionado se eliminará de forma permanente de tu lista de productos",
+                            modifier = Modifier.padding(bottom = 10.dp),
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = "¿Estás seguro de continuar?",
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(start = 45.dp, bottom = 2.dp)
+                        )
+                    }
+                },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            showDialog = false
+                            onDeleteConfirmed()
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.Black)
+                    ) {
+                        Text(text = "OK", color = Color.White)
+                    }
+                },
+                dismissButton = {
+                    Button(
+                        onClick = { showDialog = false },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+                    ) {
+                        Text(text = "CANCELAR", color = Color.White)
+                    }
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/com/delgadojuarez/ucamarket/ui/component/ProductCard.kt
+++ app/src/main/java/com/delgadojuarez/ucamarket/ui/component/ProductCard.kt
@@ -21,12 +21,17 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -69,7 +74,7 @@ fun ProductCard(
             // Imagen a la izquierda
             Image(
                 modifier = Modifier
-                    .size(105.dp)
+                    .size(115.dp)
                     .clip(shape = RoundedCornerShape(4.dp)),
                 painter = painterResource(id = R.drawable.camisa),
                 contentDescription = "Camisa")
@@ -119,29 +124,12 @@ fun ProductCard(
                             )
                         }
 
-
-                        /*Button(
-                            onClick = { /*TODO*/ },
-                            colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
-                            elevation = ButtonDefaults.buttonElevation(5.dp),
-                            modifier = Modifier.height(30.dp).padding(start = 10.dp)
-                        ) {
-                            //Text(text = "Editar", color = Color.White, fontSize = 9.sp)
-                            Icon(
-                                //modifier = Modifier.padding(end = 20.dp),
-                                imageVector = Icons.Default.Create,
-                                contentDescription = "Editar",
-                                tint = Color.White,
-                                modifier = Modifier.size(18.dp)
-                            )
-                        }*/
-
                         if(currentRoute == ScreenRoute.listProducts.route){
                             Spacer(modifier = Modifier.height(15.dp))
                             Spacer(modifier = Modifier.height(32.dp))
 
                             Button(
-                                onClick = { /*TODO*/ },
+                                onClick = { navController.navigate(ScreenRoute.EditProduct.route) },
                                 colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
                                 elevation = ButtonDefaults.buttonElevation(5.dp),
                                 modifier = Modifier.height(30.dp).padding(start = 10.dp)
@@ -152,25 +140,14 @@ fun ProductCard(
                                     imageVector = Icons.Default.Create,
                                     contentDescription = "Editar",
                                     tint = Color.White,
-                                    modifier = Modifier.size(18.dp)
-                                )
-                            }
-
-                            Button(
-                                onClick = { /*TODO*/ },
-                                colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
-                                elevation = ButtonDefaults.buttonElevation(5.dp),
-                                modifier = Modifier.height(35.dp).padding(start = 10.dp)
-                            ) {
-                                //Text(text = "Eliminar", color = Color.White, fontSize = 9.sp)
-                                Icon(
-                                    //modifier = Modifier.padding(end = 20.dp),
-                                    imageVector = Icons.Filled.Delete,
-                                    contentDescription = "Eliminar",
-                                    tint = Color.White,
                                     modifier = Modifier.size(30.dp)
                                 )
                             }
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                            DeleteProductButton()
+
                         }
 
                     }
